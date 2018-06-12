41273463eb3d1b4ea7cef1a3f117725a1d6cee92
AkshayC <chnakshay@gmail.com>
2018-06-12 18:52:08 +0530

    psne added

--- input.py
+++ input.py
@@ -7,9 +7,28 @@ def buildUserTable(emptyTable, filename):
             util_float = list(map(lambda x: float(x), util_string))
             emptyTable[key] =  util_float
 
+def findNumPlayers(filename):
+    rF = open(filename, "r")
+    for line in rF:
+        if line.startswith("# Players:"):
+            (key, val) = line.split(":	")
+            #print key + "***" + val
+            return int(val)
+        
+def findNumActions(filename):
+    rF = open(filename, "r")
+    for line in rF:
+        if line.startswith("# Actions:"):
+            (key, val) = line.split(":	")
+            actions = "a"
+            for i in line.split(" "):
+                actions = i
+            return int(actions)
+            
+        
 def buildUserTable_test():            
     dict = {}
-    game = "./random-game-test-simple"
+    game = "./games/random-game-test-simple"
     #game = "./zero-sum-game-test-simple"
     buildUserTable(dict, game)
 
@@ -18,5 +37,8 @@ def buildUserTable_test():
 
     print dict['test']
 
-    
-
+def findPlayers_test():
+    game = "./games/dominant"
+    #print "player = " + str(findNumPlayers(game))
+    print "actions =" 
+    print "asdf " + str(findNumActions(game))
